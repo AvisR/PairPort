3b57cd8e5f45d4d8187266cade627510d9d76b7e
AvisR <56581987+AvisR@users.noreply.github.com>
2021-10-23 17:28:53 +0800

    Update init.R

--- init.R
+++ init.R
@@ -1,4 +1,8 @@
-my_packages = c("randomForest", "data.table")
+my_packages = c("parallel", "foreach", "doParallel", "doSNOW", "doRNG", "tictoc", 
+           "readxl", "xts", "zoo", "lubridate", "tidyverse", "ggplot2", "dplyr",
+           "strucchange", "tseries", "fUnitRoots", "timeSeries", "exuber", 
+           "urca", "vars", "rmgarch", "FinTS", "shiny", "forecast", "ftsa", 
+           "tsDyn", "rstudioapi","PerformanceAnalytics", "gdata")
 
 install_if_missing = function(p) {
   if (p %in% rownames(installed.packages()) == FALSE) {
